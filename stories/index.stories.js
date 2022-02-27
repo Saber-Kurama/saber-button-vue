85d79216a48e0febf9423be30b5148073229144e
guochao <463561248@qq.com>
2022-02-27 20:42:36 +0800

    ✨ feat:  添加 storybook的提示

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,7 +1,8 @@
 import '../src/style';
-
+import TooltipButton from '@dangojs/saber-button-vue-test';
 export * from '../src/demo';
 
 export default {
+  component: TooltipButton,
   title: '@dangojs/saber-button-vue-test',
 };
